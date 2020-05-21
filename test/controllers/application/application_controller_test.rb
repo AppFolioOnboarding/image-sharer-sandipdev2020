6b3a19d2f64ef2b057912e72b1e06a80dc39db2e
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-21 16:23:03 -0700

    SanDev - fix test

--- test/controllers/application/application_controller_test.rb
+++ test/controllers/application/application_controller_test.rb
@@ -4,8 +4,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def test_home
     get root_path
     assert_response :ok
-    #pp response.body
-    assert_includes response.body, "Welcome to ropes"
+    assert_includes response.body, "Welcome to ropes, Sandip"
     refute_includes response.body, 'blah'
   end
 end
