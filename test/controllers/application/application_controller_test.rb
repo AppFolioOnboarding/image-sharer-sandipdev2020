f2ee8df2ef06064824d25395ed35dc6c515551f2
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-27 13:56:58 -0700

    SanDev - fix apllication controller

--- test/controllers/application/application_controller_test.rb
+++ test/controllers/application/application_controller_test.rb
@@ -4,7 +4,7 @@ class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def test_home
     get '/application/home'
     assert_response :ok
-    assert_includes response.body, "Add image"
+    assert_includes response.body, "Welcome to ropes, Sandip"
     refute_includes response.body, 'blah'
   end
 end
