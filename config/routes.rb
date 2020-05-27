37f3a0d723878dfecb40b038032db1ab8f356861
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-27 09:18:12 -0700

    SanDev - modify route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#home'
+  resources :images, only: %i[new create show index]
+  root 'images#index'
+  resources :application, only: %i[show]
 end
