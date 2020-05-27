41638697bb974ddc7a7642a5714ddeaf1cf920b5
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-27 13:56:58 -0700

    SanDev - fix apllication controller

--- test/controllers/application/application_controller_test.rb
+++ test/controllers/application/application_controller_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class ApplicationControllerTest < ActionDispatch::IntegrationTest
   def test_home
-    get root_path
+    get '/application/home'
     assert_response :ok
-    assert_includes response.body, "Add image"
+    assert_includes response.body, "Welcome to ropes, Sandip"
     refute_includes response.body, 'blah'
   end
 end
