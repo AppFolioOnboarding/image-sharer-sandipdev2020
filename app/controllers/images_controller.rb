e87e1657ba24914e78e5726f52b6b94844b653f7
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-27 09:16:10 -0700

    SanDev - add images controller

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,28 @@
+class ImagesController < ApplicationController
+
+  def index; end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      redirect_to @image
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  private
+
+  #strong params
+  def image_params
+    params.require(:image).permit(:url)
+  end
+end
