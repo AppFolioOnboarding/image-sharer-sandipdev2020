e8775c935e14090b0cae488451672b0a41050307
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-28 00:34:43 -0700

    SanDev - fix application controller routing and test

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :images
   root 'images#index'
+  resources :application
+  get '/application/show'
 end
