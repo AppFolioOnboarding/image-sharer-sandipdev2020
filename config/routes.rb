a29b0e6014e0f01136496c6be4bcf000d784d70e
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-29 11:43:12 -0700

    SanDev - modify routes for tag, delete functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :images
   root 'images#index'
+  get '/tagged', to: 'images#tagged', as: :tagged
   resources :application
   get '/application/show'
 end
