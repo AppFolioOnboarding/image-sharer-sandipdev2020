98f246ec8ae8658132df5864294acab3a2f3d52e
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-05-29 11:40:10 -0700

    SanDev - modify image model

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 VALID_URL ||= 'https://www.gstatic.com/webp/gallery3/1.png'.freeze
 INVALID_URL ||= 'https://www.google.com'.freeze
+INVALID_URL_WITH_UNSUPPORTED_PROTOCOL ||= 'smtp://www.gstatic.com/webp/gallery3/1.png'.freeze
+INVALID_URL_WITH_UNSUPPORTED_FILE_EXTENSION ||= 'https://www.gstatic.com/webp/gallery3/1.dmg'.freeze
 EMPTY_URL ||= ''.freeze
 
 class ImagesControllerTest < ActiveSupport::TestCase
@@ -15,8 +17,24 @@ class ImagesControllerTest < ActiveSupport::TestCase
     assert_not_predicate @image, :valid?
   end
 
+  def test_image_with_invaid_url_protocol
+    @image = Image.new(url: INVALID_URL_WITH_UNSUPPORTED_PROTOCOL)
+    assert_not_predicate @image, :valid?
+  end
+
+  def test_image_with_invalid_file_extension
+    @image = Image.new(url: INVALID_URL_WITH_UNSUPPORTED_FILE_EXTENSION)
+    assert_not_predicate @image, :valid?
+  end
+
   def test_image_with_invalid_url
     @image = Image.new(url: INVALID_URL)
     assert_not_predicate @image, :valid?
   end
+
+  def test_image_with_tag
+    @image = Image.new(url: VALID_URL, tag_list: %w[flower plant])
+    assert_predicate @image, :valid?
+  end
+
 end
