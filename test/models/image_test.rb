b14fc3ba9e7f75a878a0276e77ea0474b73dde01
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-01 16:31:48 -0700

    SanDev - modify model to include optional tag param

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 VALID_URL ||= 'https://www.gstatic.com/webp/gallery3/1.png'.freeze
 INVALID_URL ||= 'https://www.google.com'.freeze
 EMPTY_URL ||= ''.freeze
+VALID_TAGS ||= %w[flower plant].freeze
 
 class ImagesControllerTest < ActiveSupport::TestCase
   def test_valid_image
@@ -18,5 +19,10 @@ class ImagesControllerTest < ActiveSupport::TestCase
   def test_image_with_invalid_url
     @image = Image.new(url: INVALID_URL)
     assert_not_predicate @image, :valid?
+  end
+
+  def test_image_with_tag
+    @image = Image.new(url: VALID_URL, tag_list: VALID_TAGS)
+    assert_predicate @image, :valid?
   end
 end
