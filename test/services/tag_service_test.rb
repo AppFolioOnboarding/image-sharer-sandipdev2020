0b4b5f3acee1be8b221db7c4c8bec71803821aff
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-02 01:22:16 -0700

    SanDev - modify tag service for delete functionality

--- test/services/tag_service_test.rb
+++ test/services/tag_service_test.rb
@@ -30,9 +30,22 @@ class TagServiceTest < ActiveSupport::TestCase
     assert_equal Image.first, images[1]
   end
 
-  def test_list_image_with_multiple_tags
-    Image.create!(url: VALID_URL, tag_list: MULTIPLE_TAGS)
-    images = TagService.list_image(tags: MULTIPLE_TAGS)
-    assert_equal Image.last, images[0]
+  def test_delete_image
+    Image.create!(url: VALID_URL, tag_list: 'flower')
+    assert_difference('Image.count', -1) do
+      TagService.delete Image.last.id
+    end
+  end
+
+  def test_delete_image_when_image_not_found
+    Image.create!(url: VALID_URL, tag_list: 'flower')
+    assert_difference('Image.count', -1) do
+      TagService.delete Image.last.id
+    end
+    assert_no_difference('Image.count') do
+      assert_raises(Exception) {
+        TagService.delete Image.last.id
+      }
+    end
   end
 end
