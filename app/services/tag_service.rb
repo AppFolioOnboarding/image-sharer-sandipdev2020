5ac1f935561cb91df974d6d035bf6ee1d5964295
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-02 00:38:21 -0700

    SanDev - add tag service

--- app/services/tag_service.rb
+++ app/services/tag_service.rb
@@ -2,8 +2,6 @@ class TagService
   class << self
     def list_image(params)
       if params[:tags].present?
-        pp params[:tags].split(',')
-        pp Image.tagged_with(params[:tags].split(','))
         Image.tagged_with(params[:tags].split(',')).order('created_at DESC')
       else
         Image.all.order('created_at DESC')
