e7362623792a70dab4de8e95671c116e04597a48
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-02 01:21:37 -0700

    SanDev - modify images controller for delete functionality

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -22,10 +22,10 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_index_path_tags
-    Image.create(url: VALID_URL, tag_list: VALID_TAG)
+    Image.create(url: VALID_URL, tag_list: VALID_FLOWER_TAG)
     get images_path
     assert_select '.tag-list' do |tag|
-      assert_equal [VALID_TAG], tag.map(&:text)
+      assert_equal [VALID_FLOWER_TAG], tag.map(&:text)
     end
   end
 
@@ -48,6 +48,8 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
     assert_redirected_to image_path(Image.last)
     assert_includes Image.last.url, VALID_URL
+    follow_redirect!
+    assert_select '.alert', 'image created successfully'
   end
 
   def test_create_new_image_with_invalid_url
@@ -79,7 +81,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to image_path(Image.last)
     assert_includes Image.last.url, VALID_URL
     follow_redirect!
-    assert_select 'a[href=?]',  image_path(VALID_FLOWER_TAG)
+    assert_select 'a[href=?]',  root_path(tags: VALID_FLOWER_TAG)
   end
 
   def test_index_with_unique_tags
@@ -100,5 +102,48 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get root_path(tags: EMPTY_TAG)
     assert_select 'a[href=?]',  root_path(tags: VALID_PLANT_TAG)
     assert_select 'a[href=?]',  root_path(tags: VALID_FLOWER_TAG)
+  end
+
+  def test_index_with_unique_tags
+    assert_difference('Image.count', 2) do
+      post '/images', params: { image: { url: VALID_URL, tag_list: VALID_FLOWER_TAG } }
+      post '/images', params: { image: { url: VALID_URL, tag_list: VALID_PLANT_TAG } }
+    end
+    get root_path(tags: VALID_PLANT_TAG)
+    assert_select 'a[href=?]',  root_path(tags: VALID_PLANT_TAG)
+    assert_not_includes response.body, VALID_FLOWER_TAG
+  end
+
+  def test_index_with_empty_tags
+    assert_difference('Image.count', 2) do
+      post '/images', params: { image: { url: VALID_URL, tag_list: VALID_FLOWER_TAG } }
+      post '/images', params: { image: { url: VALID_URL, tag_list: VALID_PLANT_TAG } }
+    end
+    get root_path(tags: EMPTY_TAG)
+    assert_select 'a[href=?]',  root_path(tags: VALID_PLANT_TAG)
+    assert_select 'a[href=?]',  root_path(tags: VALID_FLOWER_TAG)
+  end
+
+  def test_image_destroy
+    Image.create!(url: VALID_URL, tag_list: VALID_FLOWER_TAG)
+    assert_difference('Image.count', -1) do
+      delete image_path(Image.last.id)
+    end
+    assert_redirected_to images_path
+    follow_redirect!
+    assert_select '.alert', 'image deleted successfully'
+  end
+
+  def test_image_destroy_raises_exception_for_already_deleted_image
+    Image.create!(url: VALID_URL, tag_list: VALID_FLOWER_TAG)
+    id = Image.last.id
+    assert_difference('Image.count', -1) do
+      delete image_path(Image.last.id)
+    end
+    assert_no_difference('Image.count') do
+      assert_raises(Exception) {
+        delete image_path(Image.last.id)
+      }
+    end
   end
 end
