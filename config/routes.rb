040c27e6b15dc20753888aa5e828b15f41f075d3
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-04 13:58:17 -0700

    SanDev - add feedback controller and api controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :images, only: %i[new create show index destroy]
   root 'images#index'
+  # application resource
   resources :application, only: %i[show]
+  # feedback resource
   resources :feedbacks, only: [:new]
-
+  # feedback api resource
   namespace :api do
     resource :feedbacks, only: [:create]
   end
