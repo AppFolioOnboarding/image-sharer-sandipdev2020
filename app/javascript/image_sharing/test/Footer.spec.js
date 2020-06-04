f1b996de9b6bbaf02f0c4d201569aa7f4156ce58
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-04 16:47:07 -0700

    SanDev - add footer related changes

---
+++ app/javascript/image_sharing/test/Footer.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+
+import Footer from '../components/Footer';
+
+describe('Footer', () => {
+  it('should display footer', () => {
+    const footerWrapper = shallow(<Footer />);
+    const footer = footerWrapper.find('footer');
+    expect(footer.text()).to.equal('Copyright: AppFolio Inc. Onboarding');
+  });
+});
