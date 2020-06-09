0c94c7f5dcab169e4fba988a30f31575e3610b9e
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-09 12:19:57 -0700

    SanDev - add feedback store

---
+++ app/javascript/image_sharing/test/stores/FeedbackStore.spec.js
@@ -0,0 +1,32 @@
+/* eslint-env mocha */
+
+import { expect } from 'chai';
+import { FeedbackStore } from '../../stores/FeedbackStore';
+
+describe('FeedbackStore', () => {
+  it('defaults to an empty name', () => {
+    const feedbackStore = new FeedbackStore();
+    expect(feedbackStore.name).to.equal('');
+  });
+
+  it('defaults to empty comments', () => {
+    const feedbackStore = new FeedbackStore();
+    expect(feedbackStore.comments).to.equal('');
+  });
+
+  describe('setComments', () => {
+    it('should update comments', () => {
+      const feedbackStore = new FeedbackStore();
+      feedbackStore.setComments('foo');
+      expect(feedbackStore.comments).to.equal('foo');
+    });
+  });
+
+  describe('setName', () => {
+    it('should update name', () => {
+      const feedbackStore = new FeedbackStore();
+      feedbackStore.setName('foo');
+      expect(feedbackStore.name).to.equal('foo');
+    });
+  });
+});
