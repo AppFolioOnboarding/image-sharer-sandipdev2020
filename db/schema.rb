8cd52114d05f789aa1b71ccbc145e4de4093d483
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-09 12:20:51 -0700

    SanDev - add model and related migration scripts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_230918) do
+ActiveRecord::Schema.define(version: 2020_06_08_192512) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string "url", null: false
