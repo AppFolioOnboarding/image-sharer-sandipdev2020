3dbba91efb746c3d131fa207d04b55f6a5edc8cc
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-12 03:38:23 -0700

    SanDev - add post feedback service

--- app/javascript/image_sharing/services/PostFeedbackService.js
+++ app/javascript/image_sharing/services/PostFeedbackService.js
@@ -1,7 +1,24 @@
-import { post } from "../utils/helper";
+import { post } from '../utils/helper';
 
-export class PostFeedbackService {
-  /* Implement your service */
+export default class PostFeedbackService {
+  constructor(store) {
+    this.store = store;
+  }
+
+  postFeedback = () => post('/api/feedbacks', { name: this.store.name, comment: this.store.comments })
+    .then((data) => {
+      this.setFlashMessage('success', data.message);
+      this.resetForm();
+    })
+    .catch(() => {
+      this.setFlashMessage('danger', 'try again');
+    });
+
+  setFlashMessage(status, message) {
+    this.store.setResponse(status, message);
+  }
+
+  resetForm() {
+    this.store.resetForm();
+  }
 }
-
-export default PostFeedbackService;
