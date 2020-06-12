7935b0746c38005b43ba9f8b4340635e2722397b
sandipdev2020 <sandipnayakat2020@gmail.com>
2020-06-12 04:08:53 -0700

    SanDev - modify feedbacks api controller

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,16 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-     # Implement your create action
+      feedback = Feedback.new(feedback_params)
+      if feedback.save
+        render status: :created, json: {message: 'Successfully saved!'}
+      else
+        render status: :internal_server_error, json: {message: 'Could not save feedback!'}
+      end
+    end
+
+    def feedback_params
+      params.require(:feedback).permit(:name, :comment)
     end
   end
 end
